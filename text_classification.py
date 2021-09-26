73b0ef9dbc31a95afeb128e456341edbeb21fb6b
Adil Baig <adilbaig@google.com>
2021-09-26 17:36:13 +0400

    Text Classification Tut

--- text_classification.py
+++ text_classification.py
@@ -48,11 +48,6 @@ def main(args):
     subset='training', 
     seed=seed)
 
-  for text_batch, label_batch in raw_train_ds.take(1):
-    for i in range(3):
-      print("Review", text_batch.numpy()[i])
-      print("Label", label_batch.numpy()[i])
-
   # Next, you will create a validation and test dataset. You will use the remaining 5,000 reviews from the training set for validation.
   raw_val_ds = tf.keras.preprocessing.text_dataset_from_directory(
     'aclImdb/train', 
@@ -74,29 +69,16 @@ def main(args):
     output_mode='int',
     output_sequence_length = sequence_length)
   
-  # Make a text-only dataset (without labels), then call adapt
-  train_text = raw_train_ds.map(lambda x, y: x)
-  vectorize_layer.adapt(train_text)
-
   def vectorize_text(text, label):
     text = tf.expand_dims(text, -1)
     return vectorize_layer(text), label
-
-  # retrieve one batch (of 32 reviews and labels) from the dataset
-  text_batch, label_batch = next(iter(raw_train_ds))
-  first_review, first_label = text_batch[0], label_batch[0]
-  print("Review", first_review)
-  print("Label", raw_train_ds.class_names[first_label])
-  print("Vectorized review", vectorize_text(first_review, first_label))
-  print("1287 ---> ",vectorize_layer.get_vocabulary()[1287])
-  print(" 313 ---> ",vectorize_layer.get_vocabulary()[313])
-  print('Vocabulary size: {}'.format(len(vectorize_layer.get_vocabulary())))
 
   # Now apply the vectorization layer to all the datasets
   train_ds = raw_train_ds.map(vectorize_text)
   val_ds   = raw_val_ds.map(vectorize_text)
   test_ds  = raw_test_ds.map(vectorize_text)
 
+  # Configure the dataset for performance
   AUTOTUNE = tf.data.AUTOTUNE
   train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
   val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
@@ -125,43 +107,12 @@ def main(args):
   loss, accuracy = model.evaluate(test_ds)
   print("Loss: {}, Accuracy: {}".format(loss, accuracy))
 
-  # Print the history of training
-  history_dict = history.history
-  acc = history_dict['binary_accuracy']
-  val_acc = history_dict['val_binary_accuracy']
-  loss = history_dict['loss']
-  val_loss = history_dict['val_loss']
-
-  epochs = range(1, len(acc) + 1)
-
-  # "bo" is for "blue dot"
-  plt.plot(epochs, loss, 'bo', label='Training loss')
-  # b is for "solid blue line"
-  plt.plot(epochs, val_loss, 'b', label='Validation loss')
-  plt.title('Training and validation loss')
-  plt.xlabel('Epochs')
-  plt.ylabel('Loss')
-  plt.legend()
-
-  plt.show()
-
-  plt.plot(epochs, acc, 'bo', label='Training acc')
-  plt.plot(epochs, val_acc, 'b', label='Validation acc')
-  plt.title('Training and validation accuracy')
-  plt.xlabel('Epochs')
-  plt.ylabel('Accuracy')
-  plt.legend(loc='lower right')
-
-  plt.show()
-
   # Export the model
   # This model exports the vectorization steps also!
   # "Including the text preprocessing logic inside your model enables you to export a model 
   # for production that simplifies deployment, and reduces the potential for train/test skew."
   export_model = tf.keras.Sequential([vectorize_layer, model, layers.Activation('sigmoid')])
-  export_model.compile(
-      loss=losses.BinaryCrossentropy(from_logits=False), optimizer="adam", metrics=['accuracy']
-  )
+  export_model.compile(loss=losses.BinaryCrossentropy(from_logits=False), optimizer="adam", metrics=['accuracy'])
 
   # Test it with `raw_test_ds`, which yields raw strings
   loss, accuracy = export_model.evaluate(raw_test_ds)
